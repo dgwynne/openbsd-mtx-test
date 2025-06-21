fb47bfbe75428be379b56c7c876364d86f06b950
David Gwynne <david@gwynne.id.au>
2025-06-21 22:40:44 +1000

    this seems to work

--- parking/mutex.c
+++ parking/mutex.c
@@ -6,21 +6,29 @@
 #include <machine/spinlock.h>
 #include <sys/queue.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#define ISSET(_w, _m) ((_w) & (_m))
 
 #define intr_disable() (0)
 #define intr_restore(m) do { } while (0)
 
+struct cpu_info;
+
+#define curcpu() ((struct cpu_info *)1)
+
 struct waiter {
-	struct mutex		*mtx;
+	struct mutex		*volatile mtx;
 	TAILQ_ENTRY(waiter)	 entry;
-};
+} __aligned(64);
 
 TAILQ_HEAD(waiters, waiter);
 
 struct mtx_park {
-	pthread_t		lock;
-	struct waiters		waiters;
-};
+	struct cpu_info		*lock;
+	struct waiters		 waiters;
+} __aligned(64);
 
 #define PARK_INITIALIZER(_park) {				\
 	.lock = NULL,						\
@@ -40,27 +48,12 @@ mtx_park(struct mutex *mtx)
 static unsigned long
 mtx_enter_park(struct mtx_park *p)
 {
-	//struct cpu_info *ci = curcpu();
-	//struct cpu_info *owner;
-	pthread_t ci = pthread_self();
-	pthread_t owner;
+	struct cpu_info * const ci = curcpu();
 	unsigned long m;
 
 	m = intr_disable();
-#if 1
-	while ((owner = atomic_cas_ptr(&p->lock, NULL, ci)) != NULL)
+	while (atomic_cas_ptr(&p->lock, NULL, ci) != 0)
 		CPU_BUSY_CYCLE();
-#else
-	for (;;) {
-		owner = p->lock;
-		if (owner == NULL) {
-			owner = atomic_cas_ptr(&p->lock, NULL, ci);
-			if (owner == NULL)
-				break;
-		}
-		CPU_BUSY_CYCLE();
-	}
-#endif
 	membar_enter_after_atomic();
 
 	return (m);
@@ -80,91 +73,80 @@ mtx_init(struct mutex *mtx)
 	mtx->mtx_owner = 0;
 }
 
-unsigned long
-mtx_enter_self(struct mutex *mtx, unsigned long self)
-{
-	unsigned long owner;
-
-#if 1
-	owner = atomic_cas_ulong(&mtx->mtx_owner, 0, self);
-	if (owner == 0)
-		membar_enter_after_atomic();
-#else
-	owner = mtx->mtx_owner;
-	if (owner == 0) {
-		owner = atomic_cas_ulong(&mtx->mtx_owner, 0, self);
-		if (owner == 0)
-			membar_enter_after_atomic();
-	}
-#endif
-
-	return (owner);
-}
-
 int
 mtx_enter_try(struct mutex *mtx)
 {
-	return (mtx_enter_self(mtx, (unsigned long)pthread_self()) == 0);
+	unsigned long self = (unsigned long)pthread_self();
+
+	if (atomic_cas_ulong(&mtx->mtx_owner, 0, self) == 0) {
+		membar_enter_after_atomic();
+		return (1);
+	}
+
+	return (0);
 }
 
 void
 mtx_enter(struct mutex *mtx)
 {
 	struct mtx_park *p;
-	struct waiter w;
+	struct waiter w, *n;
 	unsigned long self = (unsigned long)pthread_self();
 	unsigned long owner;
 	unsigned int i;
+	unsigned long m;
 
-	owner = mtx_enter_self(mtx, self);
-	if (owner == 0)
+	owner = atomic_cas_ulong(&mtx->mtx_owner, 0, self);
+	if (owner == 0) {
+		/* we got the lock first go. this is the fast path */
 		goto locked;
-
-	/* spinning++ */
-	for (i = 0; i < 40; i++) {
-		CPU_BUSY_CYCLE();
-
-		owner = mtx->mtx_owner;
-		if (owner == 0) {
-			owner = mtx_enter_self(mtx, self);
-			if (owner == 0)
-				goto spinlocked;
-		}
 	}
 
+	if (__predict_false(owner == (self | 1))) {
+		/*
+		 * panic("%s(%p): locking against myself", __func__, mtx);
+		 */
+		abort();
+	}
+
+	/* take the really slow path */
 	p = mtx_park(mtx);
-	do {
-		unsigned long nself = self;
-		unsigned long m;
 
-		atomic_cas_ulong(&mtx->mtx_owner, owner, owner | 1);
+	/* spinning++ */
+	w.mtx = mtx;
+	m = mtx_enter_park(p);
+	TAILQ_INSERT_TAIL(&p->waiters, &w, entry);
+	mtx_leave_park(p, m);
 
-		m = mtx_enter_park(p);
-		owner = mtx->mtx_owner;
-		if (owner & 1) {
-			w.mtx = mtx;
-			TAILQ_INSERT_TAIL(&p->waiters, &w, entry);
-		}
-		mtx_leave_park(p, m);
+	for (;;) {
+		unsigned long o;
 
-		if (owner & 1) {
+		o = atomic_cas_ulong(&mtx->mtx_owner, owner, owner | 1);
+		if (o == owner || ISSET(o, 1)) {
 			while (w.mtx != NULL)
 				CPU_BUSY_CYCLE();
-
-			m = mtx_enter_park(p);
-			TAILQ_REMOVE(&p->waiters, &w, entry);
-			if (!TAILQ_EMPTY(&p->waiters))
-				nself |= 1;
-			mtx_leave_park(p, m);
 		}
 
-		owner = mtx_enter_self(mtx, nself);
-	} while (owner != 0);
+		owner = atomic_cas_ulong(&mtx->mtx_owner, 0, self);
+		if (owner == 0)
+			break;
 
-spinlocked:
+		w.mtx = mtx;
+	}
+
+	m = mtx_enter_park(p);
+	TAILQ_REMOVE(&p->waiters, &w, entry);
+	TAILQ_FOREACH(n, &p->waiters, entry) {
+		if (n->mtx == mtx) {
+			mtx->mtx_owner = self | 1;
+			break;
+		}
+	}
+	mtx_leave_park(p, m);
 	/* spinning-- */
+
 locked:
-	return;
+	membar_enter_after_atomic();
 }
 
 void
@@ -180,9 +162,9 @@ mtx_leave(struct mutex *mtx)
 		unsigned long m;
 		struct waiter *w;
 
-		if ((owner & ~0x1UL) != self) {
+		if (__predict_false(owner != (self | 1))) {
 			/*
-			 * panic("%s(%p): not locked", __func__, mtx);
+			 * panic("%s(%p): not owner", __func__, mtx);
 			 */
 			abort();
 		}
