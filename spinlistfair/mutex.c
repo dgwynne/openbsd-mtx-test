551e1546600e8d89941881efa673059b469ff12a
David Gwynne <david@gwynne.id.au>
2025-06-25 08:03:57 +1000

    a completely fair spinlist variant.
    unlock moves ownership to the next thread/cpu waiting for the lock
    rather than releasing it and waking up the next thread. this means
    there's no barging, which in turn means throughput isnt great.

---
+++ spinlistfair/mutex.c
@@ -0,0 +1,93 @@
+#include <pthread.h>
+
+#include <mutex.h>
+#include "../atomic.h"
+
+struct mutex_waiter {
+	pthread_t self;
+	TAILQ_ENTRY(mutex_waiter) entry;
+};
+
+void
+mtx_init(struct mutex *mtx)
+{
+	mtx->mtx_spin = 0;
+	mtx->mtx_owner = NULL;
+	TAILQ_INIT(&mtx->mtx_waiting);
+}
+
+static void
+mtx_enter_spin(struct mutex *mtx)
+{
+	while (atomic_cas_uint(&mtx->mtx_spin, 0, 1) != 0)
+		CPU_BUSY_CYCLE();
+	membar_enter_after_atomic();
+}
+
+static void
+mtx_leave_spin(struct mutex *mtx)
+{
+	membar_exit();
+	mtx->mtx_spin = 0;
+}
+
+int
+mtx_enter_try(struct mutex *mtx)
+{
+	pthread_t self = pthread_self();
+	pthread_t owner;
+
+	mtx_enter_spin(mtx);
+	owner = mtx->mtx_owner;
+	if (owner == NULL)
+		mtx->mtx_owner = self;
+	mtx_leave_spin(mtx);
+
+	return (owner == NULL);
+}
+
+void
+mtx_enter(struct mutex *mtx)
+{
+	pthread_t self = pthread_self();
+	struct mutex_waiter w = { .self = self };
+	pthread_t owner;
+
+	mtx_enter_spin(mtx);
+	owner = mtx->mtx_owner;
+	if (owner == NULL)
+		mtx->mtx_owner = self;
+	else {
+		if (mtx->mtx_waiting.tqh_last == NULL) { /* sigh */
+			/* work around TAILQ_HEAD_INITIALIZER */
+			mtx->mtx_waiting.tqh_last =
+			    &mtx->mtx_waiting.tqh_first;
+		}
+
+		TAILQ_INSERT_TAIL(&mtx->mtx_waiting, &w, entry);
+	}
+	mtx_leave_spin(mtx);
+
+	if (owner != NULL) {
+		while (READ_ONCE(w.self))
+			CPU_BUSY_CYCLE();
+	}
+}
+
+void
+mtx_leave(struct mutex *mtx)
+{
+	struct mutex_waiter *n;
+
+	mtx_enter_spin(mtx);
+	n = TAILQ_FIRST(&mtx->mtx_waiting);
+	if (n == NULL)
+		mtx->mtx_owner = NULL;
+	else {
+		/* move ownership */
+		TAILQ_REMOVE(&mtx->mtx_waiting, n, entry);
+		mtx->mtx_owner = n->self;
+		n->self = NULL;
+	}
+	mtx_leave_spin(mtx);
+}
